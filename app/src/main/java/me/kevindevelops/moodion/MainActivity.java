d4386f5c0a0bf815edac8a330eede30c9597095d
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-05-30 22:03:23 -0400

    Added submit button, made imgBitmap a global variable and commented some code.

--- app/src/main/java/me/kevindevelops/moodion/MainActivity.java
+++ app/src/main/java/me/kevindevelops/moodion/MainActivity.java
@@ -16,11 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mButtonChoosePhoto;
     private Button mButtonTakePhoto;
+    private Button mButtonSubmit;
     private ImageView mImageViewPreview;
 
     private static final int RC_CAPTURE_IMAGE = 0;
 
     private Uri mImageUri;
+    private Bitmap imgBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         mButtonChoosePhoto = (Button) findViewById(R.id.button_choose_gallery);
         mButtonTakePhoto = (Button) findViewById(R.id.button_take_pic);
+        mButtonSubmit = (Button)findViewById(R.id.button_submit);
         mImageViewPreview = (ImageView)findViewById(R.id.iv_preview);
 
         mButtonChoosePhoto.setOnClickListener(new View.OnClickListener() {
@@ -41,8 +44,16 @@ public class MainActivity extends AppCompatActivity {
         mButtonTakePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Launches Intent for camera capture
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent,RC_CAPTURE_IMAGE);
+            }
+        });
+
+        mButtonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
@@ -51,8 +62,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK) {
+            //Gets photo in Bitmap and Uri if it was taken from camera
+            //Displays photo into the preview ImageView
             if(requestCode == RC_CAPTURE_IMAGE) {
-                Bitmap imgBitmap = (Bitmap)data.getExtras().get("data");
+                imgBitmap = (Bitmap)data.getExtras().get("data");
                 mImageUri = data.getData();
                 mImageViewPreview.setImageBitmap(imgBitmap);
             }
