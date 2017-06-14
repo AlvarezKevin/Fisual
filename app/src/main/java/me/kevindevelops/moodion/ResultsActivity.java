0af118ae2033f37eab06c6d6ae00915eb1dbfa6b
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-14 19:53:49 -0400

    Gets uri from intent and previews it into imageview

--- app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
+++ app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
@@ -1,14 +1,40 @@
 package me.kevindevelops.moodion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = ResultsActivity.class.getSimpleName();
+
+    private ImageView mPreviewIV;
+
+    private Bitmap mImageBitmap;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mPreviewIV = (ImageView)findViewById(R.id.iv_results_preview);
+
+        if(getIntent().getData() != null) {
+            mImageUri = getIntent().getData();
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mImageUri);
+            } catch (IOException e) {
+                Toast.makeText(this,"Could not get image",Toast.LENGTH_SHORT).show();
+            }
+            mPreviewIV.setImageBitmap(mImageBitmap);
+        }
     }
 }
