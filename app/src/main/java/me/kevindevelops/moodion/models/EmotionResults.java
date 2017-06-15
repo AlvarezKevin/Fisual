69f1002f8392756e5e99d8e9a88ead125bb4c975
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-15 11:03:36 -0400

    Added Model class for results

---
+++ app/src/main/java/me/kevindevelops/moodion/models/EmotionResults.java
@@ -0,0 +1,91 @@
+package me.kevindevelops.moodion.models;
+
+/**
+ * Created by Kevin on 6/14/2017.
+ */
+
+public class EmotionResults {
+    private float anger;
+    private float contempt;
+    private float disgust;
+    private float fear;
+    private float happiness;
+    private float neutral;
+    private float sadness;
+    private float surprise;
+
+    public EmotionResults(float anger, float contempt, float disgust, float fear, float happiness, float neutral, float sadness, float surprise) {
+        this.anger = anger;
+        this.contempt = contempt;
+        this.disgust = disgust;
+        this.fear = fear;
+        this.happiness = happiness;
+        this.neutral = neutral;
+        this.sadness = sadness;
+        this.surprise = surprise;
+    }
+
+    public float getAnger() {
+        return anger;
+    }
+
+    public void setAnger(float anger) {
+        this.anger = anger;
+    }
+
+    public float getContempt() {
+        return contempt;
+    }
+
+    public void setContempt(float contempt) {
+        this.contempt = contempt;
+    }
+
+    public float getDisgust() {
+        return disgust;
+    }
+
+    public void setDisgust(float disgust) {
+        this.disgust = disgust;
+    }
+
+    public float getFear() {
+        return fear;
+    }
+
+    public void setFear(float fear) {
+        this.fear = fear;
+    }
+
+    public float getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(float happiness) {
+        this.happiness = happiness;
+    }
+
+    public float getNeutral() {
+        return neutral;
+    }
+
+    public void setNeutral(float neutral) {
+        this.neutral = neutral;
+    }
+
+    public float getSadness() {
+        return sadness;
+    }
+
+    public void setSadness(float sadness) {
+        this.sadness = sadness;
+    }
+
+    public float getSurprise() {
+        return surprise;
+    }
+
+    public void setSurprise(float surprise) {
+        this.surprise = surprise;
+    }
+}
