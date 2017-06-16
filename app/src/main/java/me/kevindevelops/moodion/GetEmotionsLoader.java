c144f12c3fa16ba7292401dfe57b29bf3db76bd3
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-16 09:32:13 -0400

    Parses json and puts it into a list of EmotionResults

--- app/src/main/java/me/kevindevelops/moodion/GetEmotionsLoader.java
+++ app/src/main/java/me/kevindevelops/moodion/GetEmotionsLoader.java
@@ -6,9 +6,13 @@ import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.HttpEntity;
@@ -45,6 +49,7 @@ public class GetEmotionsLoader extends AsyncTaskLoader<List<EmotionResults>> {
     @Override
     public List<EmotionResults> loadInBackground() {
         HttpClient httpClient = new DefaultHttpClient();
+        String json = null;
         try {
             HttpPost httpPost = new HttpPost("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
             httpPost.setHeader("Content-Type", "application/octet-stream");
@@ -63,13 +68,42 @@ public class GetEmotionsLoader extends AsyncTaskLoader<List<EmotionResults>> {
             HttpEntity httpEntity = response.getEntity();
 
             if (httpClient != null) {
-                Log.v(LOG_TAG, EntityUtils.toString(httpEntity));
+                json = EntityUtils.toString(httpEntity);
+                Log.v(LOG_TAG, json);
             }
         } catch (Exception e) {
             Log.v(LOG_TAG, "Couldn't get emotions", e);
         }
 
-        return null;
+        try{
+            return getScoresFromJson(json);
+        }catch (JSONException e) {
+            Log.v(LOG_TAG,"Couldn't parse json");
+            return null;
+        }
+    }
+
+    public List<EmotionResults> getScoresFromJson(String json) throws JSONException{
+        List<EmotionResults> list = new ArrayList<EmotionResults>();
+        JSONArray jsonArray = new JSONArray(json);
+        for(int i = 0;i < jsonArray.length();i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            JSONObject scores = jsonObject.getJSONObject("scores");
+
+            EmotionResults tempEmotions = new EmotionResults();
+
+            tempEmotions.setAnger(scores.getDouble("anger"));
+            tempEmotions.setContempt(scores.getDouble("contempt"));
+            tempEmotions.setDisgust(scores.getDouble("disgust"));
+            tempEmotions.setFear(scores.getDouble("fear"));
+            tempEmotions.setHappiness(scores.getDouble("happiness"));
+            tempEmotions.setNeutral(scores.getDouble("neutral"));
+            tempEmotions.setSadness(scores.getDouble("sadness"));
+            tempEmotions.setSurprise(scores.getDouble("surpise"));
+
+            list.add(tempEmotions);
+        }
+        return list;
     }
 
 }
