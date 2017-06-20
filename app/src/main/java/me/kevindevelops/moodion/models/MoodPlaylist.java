188725d4961de8863cf3e7c696856b51c371b169
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-20 13:42:24 -0400

    Models for playlists and tracks

---
+++ app/src/main/java/me/kevindevelops/moodion/models/MoodPlaylist.java
@@ -0,0 +1,110 @@
+package me.kevindevelops.moodion.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kevin on 6/19/2017.
+ */
+
+public class MoodPlaylist {
+
+    private String emotion;
+    private String name;
+    private String ownerId;
+    private String id;
+    private String url;
+    private String apiUrl;
+    private String tracksUrl;
+    private int amountOfTracks;
+    private String imageUrl;
+    private List<Track> tracksList;
+
+    public MoodPlaylist() {
+        tracksList = new ArrayList<Track>();
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public void setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
+
+    public String getTracksUrl() {
+        return tracksUrl;
+    }
+
+    public void setTracksUrl(String tracksUrl) {
+        this.tracksUrl = tracksUrl;
+    }
+
+    public int getAmountOfTracks() {
+        return amountOfTracks;
+    }
+
+    public void setAmountOfTracks(int amountOfTracks) {
+        this.amountOfTracks = amountOfTracks;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<Track> getTracksList() {
+        return tracksList;
+    }
+
+    public void setTracksList(List<Track> tracksList) {
+        this.tracksList = tracksList;
+    }
+
+    public void addTrack(Track track) {
+        this.tracksList.add(track);
+    }
+}
---
+++ app/src/main/java/me/kevindevelops/moodion/models/Track.java
@@ -0,0 +1,15 @@
+package me.kevindevelops.moodion.models;
+
+import java.util.List;
+
+/**
+ * Created by Kevin on 6/20/2017.
+ */
+
+public class Track {
+    private String imageUrl;
+    private String trackName;
+    private String songUri;
+    private List<String> artistName;
+    private String songId;
+}
