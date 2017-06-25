da5f4903e7a41d30a724ca6771adf021a573c68f
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-25 00:29:40 -0400

    Edited playlist and track models

--- app/src/main/java/me/kevindevelops/moodion/models/Track.java
+++ app/src/main/java/me/kevindevelops/moodion/models/Track.java
@@ -1,5 +1,6 @@
 package me.kevindevelops.moodion.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,4 +13,57 @@ public class Track {
     private String songUri;
     private List<String> artistName;
     private String songId;
+    private String songURL;
+
+    public Track() {
+        artistName = new ArrayList<>();
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getSongUri() {
+        return songUri;
+    }
+
+    public void setSongUri(String songUri) {
+        this.songUri = songUri;
+    }
+
+    public List<String> getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(List<String> artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getSongId() {
+        return songId;
+    }
+
+    public void setSongId(String songId) {
+        this.songId = songId;
+    }
+
+    public String getSongURL() {
+        return songURL;
+    }
+
+    public void setSongURL(String songURL) {
+        this.songURL = songURL;
+    }
 }
