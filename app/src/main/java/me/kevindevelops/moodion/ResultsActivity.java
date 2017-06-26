0388d17b3119c56b42591e1eb2b62c58abdab64d
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-26 12:24:47 -0400

    Emotions now are in a recyclerview and the top linearlayout background is the image

--- app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
+++ app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
@@ -1,20 +1,24 @@
 package me.kevindevelops.moodion;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import me.kevindevelops.moodion.adapter.EmotionsAdapter;
 import me.kevindevelops.moodion.models.EmotionResults;
 import me.kevindevelops.moodion.models.MoodPlaylist;
 
@@ -24,8 +28,10 @@ public class ResultsActivity extends AppCompatActivity {
     private static final int EMOTIONS_LOADER = 201;
     private static final int PLAYLIST_LOADER = 202;
 
-    private ImageView mPreviewIV;
-    private TextView mScoresTV;
+    private LinearLayout mTopLinearLayout;
+    private ImageView mStrongestEmotionIV;
+    private RecyclerView mEmotionsRV;
+    private RecyclerView mPlaylistRV;
 
     private Bitmap mImageBitmap;
     private Uri mImageUri;
@@ -38,8 +44,10 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mPreviewIV = (ImageView) findViewById(R.id.iv_results_preview);
-        mScoresTV = (TextView) findViewById(R.id.tv_scores);
+        mTopLinearLayout = (LinearLayout)findViewById(R.id.top_linear_layout);
+        mStrongestEmotionIV = (ImageView)findViewById(R.id.iv_strongest_emotion);
+        mEmotionsRV = (RecyclerView)findViewById(R.id.rv_emotions);
+        //mPlaylistRV = (RecyclerView)findViewById(R.id.rv_playlist);
 
         ACCESS_TOKEN = getIntent().getStringExtra(MainActivity.EXTRA_TOKEN);
         if (getIntent().getData() != null) {
@@ -49,8 +57,11 @@ public class ResultsActivity extends AppCompatActivity {
             } catch (IOException e) {
                 Toast.makeText(this, "Could not get image", Toast.LENGTH_SHORT).show();
             }
-            mPreviewIV.setImageBitmap(mImageBitmap);
         }
+
+        mTopLinearLayout.setBackground(new BitmapDrawable(mImageBitmap));
+        mEmotionsRV.setLayoutManager(new LinearLayoutManager(this));
+        mEmotionsRV.setAdapter(null);
 
         getSupportLoaderManager().initLoader(EMOTIONS_LOADER, null, emotionLoaderListener);
     }
@@ -63,25 +74,20 @@ public class ResultsActivity extends AppCompatActivity {
 
         @Override
         public void onLoadFinished(Loader<List<EmotionResults>> loader, List<EmotionResults> data) {
-            mScoresTV.append("Your results\n\n");
+            mEmotionsRV.setAdapter(new EmotionsAdapter(ResultsActivity.this, data));
 
             for (EmotionResults scores : data) {
-                mScoresTV.append("Anger : " + scores.getAnger() + "\n");
-                mScoresTV.append("Contempt : " + scores.getContempt() + "\n");
-                mScoresTV.append("Disgust : " + scores.getDisgust() + "\n");
-                mScoresTV.append("Fear : " + scores.getFear() + "\n");
-                mScoresTV.append("Happiness : " + scores.getHappiness() + "\n");
-                mScoresTV.append("Neutral : " + scores.getNeutral() + "\n");
-                mScoresTV.append("Sadness : " + scores.getSadness() + "\n");
-                mScoresTV.append("Surprise : " + scores.getSurprise() + "\n\n");
-
 
                 Map.Entry<Double,String> temp = scores.getMaxEmotion();
                 if(mStrongestEmotion == null) {
                     mStrongestEmotion = temp;
-                }else if(mStrongestEmotion.getKey().compareTo(temp.getKey()) < 0 ) {
+                }else if(mStrongestEmotion.getKey().compareTo(temp.getKey()) > 0 ) {
                     mStrongestEmotion = temp;
                 }
+            }
+
+            if(Utilities.getEmotionDrawable(mStrongestEmotion.getValue()) != 0) {
+                mStrongestEmotionIV.setImageResource(Utilities.getEmotionDrawable(mStrongestEmotion.getValue()));
             }
 
 
@@ -90,7 +96,7 @@ public class ResultsActivity extends AppCompatActivity {
 
         @Override
         public void onLoaderReset(Loader<List<EmotionResults>> loader) {
-            mScoresTV.setText("");
+            mEmotionsRV.setAdapter(null);
         }
     };
 
