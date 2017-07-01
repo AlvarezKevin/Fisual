54aedfab50bc8ea6155280d7d6a030085e253f6e
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-07-01 14:38:12 -0400

    Opens spotify on playlist info clicked

--- app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
+++ app/src/main/java/me/kevindevelops/moodion/ResultsActivity.java
@@ -1,5 +1,6 @@
 package me.kevindevelops.moodion;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
@@ -11,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -31,10 +33,12 @@ import me.kevindevelops.moodion.models.MoodPlaylist;
 public class ResultsActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = ResultsActivity.class.getSimpleName();
+
     private static final int EMOTIONS_LOADER = 201;
     private static final int PLAYLIST_LOADER = 202;
 
     private LinearLayout mTopLinearLayout;
+    private LinearLayout mPlaylistLinearLayout;
     private ImageView mStrongestEmotionIV;
     private ImageView mPlaylistIV;
     private TextView mPlaylistNameTV;
@@ -43,10 +47,13 @@ public class ResultsActivity extends AppCompatActivity {
     private RecyclerView mEmotionsRV;
     private RecyclerView mPlaylistRV;
 
+    private EmotionsAdapter mEmotionsAdapter;
+    private PlaylistAdapter mPlaylistAdapter;
+
     private Bitmap mImageBitmap;
     private Uri mImageUri;
     private String ACCESS_TOKEN;
-    private Map.Entry<Double,String> mStrongestEmotion;
+    private Map.Entry<Double, String> mStrongestEmotion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,14 +61,15 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mTopLinearLayout = (LinearLayout)findViewById(R.id.top_linear_layout);
-        mStrongestEmotionIV = (ImageView)findViewById(R.id.iv_strongest_emotion);
+        mTopLinearLayout = (LinearLayout) findViewById(R.id.top_linear_layout);
+        mPlaylistLinearLayout = (LinearLayout) findViewById(R.id.playlist_info_linear_layout);
+        mStrongestEmotionIV = (ImageView) findViewById(R.id.iv_strongest_emotion);
         mPlaylistIV = (ImageView) findViewById(R.id.iv_playlist_image);
-        mEmotionsRV = (RecyclerView)findViewById(R.id.rv_emotions);
-        mPlaylistRV = (RecyclerView)findViewById(R.id.rv_playlist);
+        mEmotionsRV = (RecyclerView) findViewById(R.id.rv_emotions);
+        mPlaylistRV = (RecyclerView) findViewById(R.id.rv_playlist);
         mPlaylistNameTV = (TextView) findViewById(R.id.tv_playlist_name);
         mPlaylistOwnerTV = (TextView) findViewById(R.id.tv_owner);
-        mAmountofTracksTV = (TextView)findViewById(R.id.tv_number_of_tracks);
+        mAmountofTracksTV = (TextView) findViewById(R.id.tv_number_of_tracks);
 
         ACCESS_TOKEN = getIntent().getStringExtra(MainActivity.EXTRA_TOKEN);
         if (getIntent().getData() != null) {
@@ -85,28 +93,29 @@ public class ResultsActivity extends AppCompatActivity {
     private LoaderManager.LoaderCallbacks<List<EmotionResults>> emotionLoaderListener = new LoaderManager.LoaderCallbacks<List<EmotionResults>>() {
         @Override
         public Loader<List<EmotionResults>> onCreateLoader(int id, Bundle args) {
-            return new GetEmotionsLoader(ResultsActivity.this,mImageBitmap);
+            return new GetEmotionsLoader(ResultsActivity.this, mImageBitmap);
         }
 
         @Override
         public void onLoadFinished(Loader<List<EmotionResults>> loader, List<EmotionResults> data) {
-            mEmotionsRV.setAdapter(new EmotionsAdapter(ResultsActivity.this, data));
+            mEmotionsAdapter = new EmotionsAdapter(ResultsActivity.this, data);
+            mEmotionsRV.setAdapter(mEmotionsAdapter);
 
             for (EmotionResults scores : data) {
 
-                Map.Entry<Double,String> temp = scores.getMaxEmotion();
-                if(mStrongestEmotion == null) {
+                Map.Entry<Double, String> temp = scores.getMaxEmotion();
+                if (mStrongestEmotion == null) {
                     mStrongestEmotion = temp;
-                }else if(mStrongestEmotion.getKey().compareTo(temp.getKey()) < 0 ) {
+                } else if (mStrongestEmotion.getKey().compareTo(temp.getKey()) < 0) {
                     mStrongestEmotion = temp;
                 }
             }
 
-            if(Utilities.getEmotionDrawable(mStrongestEmotion.getValue()) != 0) {
+            if (Utilities.getEmotionDrawable(mStrongestEmotion.getValue()) != 0) {
                 mStrongestEmotionIV.setImageResource(Utilities.getEmotionDrawable(mStrongestEmotion.getValue()));
             }
 
-            Log.v(LOG_TAG,mStrongestEmotion.getValue());
+            Log.v(LOG_TAG, mStrongestEmotion.getValue());
 
             getSupportLoaderManager().initLoader(PLAYLIST_LOADER, null, playlistLoaderListener);
         }
@@ -120,12 +129,12 @@ public class ResultsActivity extends AppCompatActivity {
     private LoaderManager.LoaderCallbacks<MoodPlaylist> playlistLoaderListener = new LoaderManager.LoaderCallbacks<MoodPlaylist>() {
         @Override
         public Loader<MoodPlaylist> onCreateLoader(int id, Bundle args) {
-            return new GetPlaylistLoader(ResultsActivity.this,ACCESS_TOKEN,mStrongestEmotion.getValue());
+            return new GetPlaylistLoader(ResultsActivity.this, ACCESS_TOKEN, mStrongestEmotion.getValue());
         }
 
         @Override
-        public void onLoadFinished(Loader<MoodPlaylist> loader, MoodPlaylist data) {
-            if(data.getImageUrl() != null) {
+        public void onLoadFinished(Loader<MoodPlaylist> loader, final MoodPlaylist data) {
+            if (data.getImageUrl() != null) {
                 Glide.with(ResultsActivity.this)
                         .load(data.getImageUrl())
                         .into(mPlaylistIV);
@@ -135,10 +144,20 @@ public class ResultsActivity extends AppCompatActivity {
             mPlaylistOwnerTV.setText(data.getOwnerId());
             mAmountofTracksTV.setText(data.getAmountOfTracks() + " Tracks");
 
-            mPlaylistRV.setAdapter(new PlaylistAdapter(ResultsActivity.this,data));
+
+            mPlaylistAdapter = new PlaylistAdapter(ResultsActivity.this, data);
+            mPlaylistRV.setAdapter(mPlaylistAdapter);
             ViewGroup.LayoutParams layoutParams = mPlaylistRV.getLayoutParams();
-            layoutParams.height = data.getAmountOfTracks() * 204;
+            layoutParams.height = data.getAmountOfTracks() * 200;
             mPlaylistRV.setLayoutParams(layoutParams);
+
+            mPlaylistLinearLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data.getUri()));
+                    startActivity(intent);
+                }
+            });
         }
 
         @Override
