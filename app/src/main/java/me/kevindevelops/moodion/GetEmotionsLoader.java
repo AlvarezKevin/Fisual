dd2692051fffac0086ff9e624df2834a63cd6de1
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-07-11 21:52:41 -0400

    Documented code

--- app/src/main/java/me/kevindevelops/moodion/GetEmotionsLoader.java
+++ app/src/main/java/me/kevindevelops/moodion/GetEmotionsLoader.java
@@ -40,56 +40,69 @@ public class GetEmotionsLoader extends AsyncTaskLoader<List<EmotionResults>> {
 
     }
 
+    // Makes sure load in background starts
     @Override
     protected void onStartLoading() {
         super.onStartLoading();
         forceLoad();
     }
 
+    // Fetches the emotions based on the image and organizes it
     @Override
     public List<EmotionResults> loadInBackground() {
         HttpClient httpClient = new DefaultHttpClient();
         String json = null;
         try {
+
+            // Create http post call with headers passing the Microsoft Emotion API key and the content type
             HttpPost httpPost = new HttpPost("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
             httpPost.setHeader("Content-Type", "application/octet-stream");
             httpPost.setHeader("Ocp-Apim-Subscription-Key", APIKEY.KEY);
 
-
+            // Converts the image from bitmap to ByteArrayInputStream so it can be passed in the http post call
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.JPEG,0,outputStream);
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 0, outputStream);
             ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
 
+            // Passes Entity including the byte array input stream of the image
             ByteArrayEntity entity = new ByteArrayEntity(IOUtils.toByteArray(inputStream));
-
             httpPost.setEntity(entity);
 
+            // Executes http post
             HttpResponse response = httpClient.execute(httpPost);
             HttpEntity httpEntity = response.getEntity();
 
+            // Attempts to read and collect json
             if (httpEntity != null) {
                 json = EntityUtils.toString(httpEntity);
-                Log.v(LOG_TAG, json);
             }
         } catch (Exception e) {
             Log.v(LOG_TAG, "Couldn't get emotions", e);
         }
 
-        try{
+        // Attempts to parse json to organize emotions of image
+        try {
             return getScoresFromJson(json);
-        }catch (JSONException e) {
-            Log.v(LOG_TAG,"Couldn't parse json");
+        } catch (JSONException e) {
+            Log.v(LOG_TAG, "Couldn't parse json");
             return null;
         }
     }
 
-    public List<EmotionResults> getScoresFromJson(String json) throws JSONException{
+
+    public List<EmotionResults> getScoresFromJson(String json) throws JSONException {
+
+        // Converts json string to a json array object
         List<EmotionResults> list = new ArrayList<EmotionResults>();
         JSONArray jsonArray = new JSONArray(json);
-        for(int i = 0;i < jsonArray.length();i++) {
+
+        // Loops through json array object and parses it
+        for (int i = 0; i < jsonArray.length(); i++) {
+
             JSONObject jsonObject = jsonArray.getJSONObject(i);
             JSONObject scores = jsonObject.getJSONObject("scores");
 
+            // Creates an EmotionResults object and passes results into it and adds it to a list
             EmotionResults tempEmotions = new EmotionResults();
 
             tempEmotions.setAnger(scores.getDouble("anger"));
